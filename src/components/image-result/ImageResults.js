ccab37d4cfe15d190891390bb103987f2e3b9b49
selvaonline <selvaonline@gmail.com>
2018-04-22 22:20:48 -0400

    initial commit

---
+++ src/components/image-result/ImageResults.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import zoomIn from 'material-ui/svg-icons/action/zoom-in';
+
+
+ class ImageResults extends Component {
+   state = {
+     open : false,
+     currentImg: ''
+  }
+
+  handleOpen = img => {
+    this.setState({open:true, currentImg:img});
+  }
+
+  handleClose = img => {
+    this.setState({open:false, currentImg:img});
+  }
+  
+  saveImage = img => {
+    this.setState({open:true, currentImg:img});   
+  }
+
+  render() {
+    let imagelistContent;
+    const {images} = this.props;
+
+    if(images) {
+      imagelistContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile
+              title = {img.tags}
+              key = {img.id}
+              subtitle = {
+                <span>
+                  by <strong>{img.user} </strong>
+                 </span>
+              }
+              actionIcon ={
+                <IconButton onClick={()=>this.handleOpen(img.largeImageURL)}>
+                    <ZoomIn color="white" />
+                  </IconButton>
+              }
+            >
+            <img src={img.largeImageURL} alt="" />
+              
+            </GridTile>
+          ))}
+        </GridList>
+      )
+    } else {
+      imagelistContent = null;
+    }
+    const actions = [
+        <FlatButton label="Download" primary={false} onClick = {this.saveImage} />,
+        <FlatButton label="Close" primary={true} onClick = {this.handleClose} />
+    ]
+    return (
+      <div>
+        {imagelistContent}
+        <Dialog
+            actions = {actions}
+            modal = {false}
+            open = {this.state.open}           
+            onRequestClose = {this.handleClose}
+        >
+        <img src = {this.state.currentImg} alt="" style ={{width:'100%'}} />
+
+        </Dialog>
+
+      </div>
+    )
+  }
+}
+ImageResults.propTypes = {
+  images : PropTypes.array.isRequired
+}
+
+export default ImageResults;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Author = `<a href='https://linkedin.com/in/selvaonline'> Selvakumar Murugesan </a>`;
+const NavBar = () => <AppBar title="Free Image Search by Selvakumar Murugesan (@selvaonline)"/>;
+
+export default NavBar;
